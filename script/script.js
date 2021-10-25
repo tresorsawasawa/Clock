ae2c482cf08cdf546e9e55b24dd2d795159bee69
tresorsawasawa <tresorsawasawa@gmail.com>
2021-10-25 10:19:26 +0200

    update script.js

--- script/script.js
+++ script/script.js
@@ -3,6 +3,6 @@ setInterval(setClock, 1000);
 function setClock() {
     const currentDate = new Date(),
         secondsRatio = currentDate.getSeconds() / 60,
-        secondsRatio = currentDate.getSeconds() / 60,
-        secondsRatio = currentDate.getSeconds() / 60;
+        minutesRatio = currentDate.getMinutes() / 60,
+        hoursRatio = currentDate.getHours() / 60;
 }
