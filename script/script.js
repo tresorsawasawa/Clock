0f6e151f9ef1517e22c5086a4d6d7003aeadf5d3
tresorsawasawa <tresorsawasawa@gmail.com>
2021-10-25 10:46:10 +0200

    update script.js

--- script/script.js
+++ script/script.js
@@ -9,4 +9,11 @@ function setClock() {
         secondsRatio = currentDate.getSeconds() / 60,
         minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60,
         hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(elem, rotationRatio) {
+    elem.style.setProperty('--rotation', rotationRatio * 360);
 }
