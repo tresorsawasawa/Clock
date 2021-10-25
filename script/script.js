1eaf0973383fa0866a7c008b41ca8c596d2004e1
tresorsawasawa <tresorsawasawa@gmail.com>
2021-10-25 11:34:43 +0200

    update script.js

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
-// setInterval(setClock, 1000);
+setInterval(setClock, 1000);
 
-// const hourHand = document.querySelector('[data-hour-hand]'),
-//     minuteHand = document.querySelector('[data-minute-hand'),
-//     secondHand = document.querySelector('[data-second-hand]');
+const hourHand = document.querySelector('[data-hour-hand]'),
+    minuteHand = document.querySelector('[data-minute-hand'),
+    secondHand = document.querySelector('[data-second-hand]');
 
-// function setClock() {
-//     const currentDate = new Date(),
-//         secondsRatio = currentDate.getSeconds() / 60,
-//         minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60,
-//         hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
-//     setRotation(secondHand, secondsRatio)
-//     setRotation(minuteHand, minutesRatio)
-//     setRotation(hourHand, hoursRatio)
-// }
+function setClock() {
+    const currentDate = new Date(),
+        secondsRatio = currentDate.getSeconds() / 60,
+        minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60,
+        hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
 
-// function setRotation(element, rotationRatio) {
-//     element.style.setProperty('--rotation', rotationRatio * 360)
-// }
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
 
-// setClock();
+setClock();
