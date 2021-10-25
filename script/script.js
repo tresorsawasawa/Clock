c9dbe6a43f7dcb40eba62db735cefc726ad9bd06
tresorsawasawa <tresorsawasawa@gmail.com>
2021-10-25 12:12:52 +0200

    update script.js

--- script/script.js
+++ script/script.js
@@ -23,9 +23,30 @@ setClock();
 // Functionality for Digital Clock 
 
 function showTime() {
-    let date = new Date(),
-        hour = getHours(),
-        minute = getMinutes(),
-        second = getSeconds();
+    var date = new Date(),
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        second = date.getSeconds(),
+        session = "AM";
+
+    if (hour == 0) {
+        hour = 12;
+    }
+
+    if (hour > 12) {
+        hour -= 12;
+        session = "PM";
+    }
+
+    hour = (hour < 10) ? '0' + hour : hour;
+    minute = (minute < 10) ? '0' + minute : minute;
+    second = (second < 10) ? '0' + second : second;
+
+    let time = hour + ":" + minute + ":" + second + " " + session;
+    document.querySelector('#time-header').innerHTML = time;
+    document.querySelector('#time-header').textContent = time;
+
+    setTimeout(showTime, 1000);
 }
+
 showTime();
